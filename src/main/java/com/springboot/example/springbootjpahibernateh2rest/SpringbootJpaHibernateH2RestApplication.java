c700e8f60070cf3ad2aaa3678fefdc7f3de3e9a0
JacquelineRP <jacqueline.rodriguez.perez@gmail.com>
2019-03-15 23:14:18 +0000

    Initial commit

---
+++ src/main/java/com/springboot/example/springbootjpahibernateh2rest/SpringbootJpaHibernateH2RestApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.example.springbootjpahibernateh2rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaHibernateH2RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaHibernateH2RestApplication.class, args);
+	}
+
+}
